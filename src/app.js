4d62162118addf074dc8a5043c5e946c48c78aa1
Kurt Schneider <k.w.schneid@gmail.com>
2017-03-13 16:13:12 -0400

    Added index changes

--- src/app.js
+++ src/app.js
@@ -1,1 +1,14 @@
-console.log('it works!');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+	render(){
+		return (
+			<div>
+			
+			</div>
+		)
+	}
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
---
+++ src/searchbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Searchbar extends React.Component{
+	render() {
+		return (
+			<div>
+				<input type="text" placeholder="search here!" />
+			</div>
+		)
+	}
+}
