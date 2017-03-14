2c4005b6cb434823a101f3ba7c37fcdfb7ee26c1
Kurt William <k.w.schneid@gmail.com>
2017-03-14 13:45:59 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,12 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+	constructor() {
+		super();
+	}
+	componentWillMount(){
+		console.log('Mounting');
+	}
 	render(){
 		return (
 			<div>
-			
+				<h1>Hello!</h1>
 			</div>
 		)
+	}
+	componentDidMount(){
+		console.log('Mounted');
 	}
 }
 
